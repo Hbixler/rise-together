4263b015deea0ee366946fc8c1fe711e9618a64d
shonda2 <shonda@unomaha.edu>
2021-04-14 14:47:41 -0500

    made a table thing

---
+++ HousingTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  HousingTableViewCell.swift
+//  Rise Together
+//
+//  Created by Solana Honda on 4/14/21.
+//
+
+import UIKit
+
+class HousingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
