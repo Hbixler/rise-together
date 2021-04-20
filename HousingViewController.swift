17d8f210b14c316229854a9d82862bcb85b3f33e
Hbixler <hbixler@unomaha.edu>
2021-04-20 18:07:05 -0500

    stuff

--- HousingViewController.swift
+++ HousingViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class HousingViewController: UIViewController, UITableViewDataSource {
+class HousingViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     var addys: [String] = []
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -15,6 +15,7 @@ class HousingViewController: UIViewController, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.register(UINib(nibName: "HousingTableViewCell", bundle: nil), forCellReuseIdentifier: "housingCell")
         let cell = tableView.dequeueReusableCell(withIdentifier: "housingCell") as! HousingTableViewCell
         cell.passIndex(index: indexPath.row)
         return cell
@@ -25,7 +26,7 @@ class HousingViewController: UIViewController, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
-
+        tableView.delegate = self
         // Do any additional setup after loading the view.
     }
     
--- JobListingsViewController.swift
+++ JobListingsViewController.swift
@@ -13,6 +13,7 @@ class JobListingsViewController: UIViewController, UITableViewDelegate, UITableV
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        tableView.register(UINib(nibName: "JobListingsTableViewCell", bundle: nil), forCellReuseIdentifier: "jobListingsCell")
         let cell = tableView.dequeueReusableCell(withIdentifier: "jobListingsCell") as! JobListingsTableViewCell
         cell.passIndex(index: indexPath.row)
         return cell
