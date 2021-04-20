4cb328054fe3e7dd393e3935b5d37c73394f3786
jdavies200 <joshuadavies@unomaha.edu>
2021-04-20 18:50:39 -0500

    calendar label

--- CalendarViewController.swift
+++ CalendarViewController.swift
@@ -16,7 +16,9 @@ class CalendarViewController: UIViewController, UICollectionViewDataSource {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "calendarCell", for: indexPath)
         return cell
     }
-    
+    public var dates = ["April 2021", "May 2021", "June 2021", "July 2021", "Aug 2021", "Sept 2021", "Oct 2021", "Nov 2021", "Dec 2021", ]
+    var datechange = 0
+
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func viewDidLoad() {
@@ -24,9 +26,25 @@ class CalendarViewController: UIViewController, UICollectionViewDataSource {
         collectionView.dataSource = self
         // Do any additional setup after loading the view.
     }
+    @IBOutlet weak var dateLabel: UILabel!
     
+    @IBAction func rightButtonPressed(_ sender: Any) {
+        if datechange < (dates.count - 1) {
+            datechange = datechange + 1
+            dateLabel.text = dates[datechange]
+            
+        }
+    }
+    
+    @IBAction func leftButtonPressed(_ sender: Any) {
 
-    /*
+        if datechange > 0 {
+            datechange = datechange - 1
+            dateLabel.text = dates[datechange]
+                   
+               }
+    }
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -34,6 +52,6 @@ class CalendarViewController: UIViewController, UICollectionViewDataSource {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-    */
+    
 
 }
