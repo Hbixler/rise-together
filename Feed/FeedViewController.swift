cb7f96fa7e863eb1c273f647e149c366382e3a84
noajurgens <noajurgens@unomaha.edu>
2021-04-22 19:02:59 -0500

    putting the array info into cells

--- Feed/FeedViewController.swift
+++ Feed/FeedViewController.swift
@@ -8,28 +8,10 @@
 import UIKit
 
 class FeedViewController: UIViewController {
-    var username: [String] = []
-    var caption: [String] = []
-    var picture: [UIImage] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        username.append("PamBeasly")
-        username.append("JimHalpert")
-        username.append("MichaelScott")
-        username.append("KevinMalone")
-        username.append("KellyKapoore")
         
-        caption.append("I've been loving my volunteer postition at Open Door Mission. If anyone is interested in joining me, let me know!")
-        caption.append("I just bought my first house! So thankful!")
-        caption.append("It's been one year since I planted this tree, look how much it has grown!")
-        caption.append("My kiddo just started fifth grade! So glad I got to drop him off on this special day!")
-        caption.append("Just got a job at Burger King!")
-        
-        picture.append(UIImage(named: "post1")!)
-        picture.append(UIImage(named: "post2")!)
-        picture.append(UIImage(named: "post3")!)
-        picture.append(UIImage(named: "post4")!)
-        picture.append(UIImage(named: "post5")!)
         // Do any additional setup after loading the view.
     }
     
